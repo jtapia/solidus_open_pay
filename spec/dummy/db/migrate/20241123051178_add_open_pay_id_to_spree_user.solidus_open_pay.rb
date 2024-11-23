e66143313ee7dd232cd4c33e9977ae6c3b2f66ef
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2024-11-22 23:24:15 -0600

    Remove unnecessary code

--- spec/dummy/db/migrate/20241123051178_add_open_pay_id_to_spree_user.solidus_open_pay.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# This migration comes from solidus_open_pay (originally 20241119003957)
-class AddOpenPayIdToSpreeUser < ActiveRecord::Migration[7.1]
-  def change
-    add_column :spree_users, :open_pay_id, :string
-  end
-end
