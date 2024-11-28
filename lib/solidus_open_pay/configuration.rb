33afeb07e2127e8ccbb0a81fc7ad20260653122a
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2024-11-28 02:19:20 -0600

    Add minor enhancements

--- lib/solidus_open_pay/configuration.rb
+++ lib/solidus_open_pay/configuration.rb
@@ -2,15 +2,6 @@
 
 module SolidusOpenPay
   class Configuration
-    attr_accessor :installment_options, :installment_default
-
-    # def installment_options
-    #   @installment_options ||= :installment_options
-    # end
-
-    # def installment_default
-    #   @installment_default ||= :installment_default
-    # end
   end
 
   class << self
